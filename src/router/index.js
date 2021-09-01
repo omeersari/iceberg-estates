4fad32a43530d21367710a0f21cdc21d4d74620d
omeersari <omeersari@gmail.com>
2021-09-01 19:18:14 +0300

    homepage is started. axios is installed. scss files are created.

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home,
   },
   {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue")
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div class="homepage">
+      <div class="login-card">
+        <div class="logo">
+          <p>ICEBERG ESTATES</p>
+        </div>
+        <div class="login-form">
+          <input type="text" placeholder="Username">
+          <input type="password" placeholder="Password">
+          <button @click="gotoHomepage">LOGIN</button>
+        </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  async created() {
+    const res = await this.$http.post(process.env.VUE_APP_API, {"fields": {
+    "appointment_date": "2020-10-14T00:03:00.000Z"
+}})
+    console.log(res.data)
   },
+  methods: {
+    gotoHomepage() {
+      this.$router.push('/dashboard')
+    }
+  }
 };
 </script>
