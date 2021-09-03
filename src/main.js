cae0ef253e37ff883a4ab73ab15ba817f270bcce
omeersari <omeersari@gmail.com>
2021-09-03 22:48:12 +0300

    moment.js, paginate and calendar libraries are added.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from 'axios';
+import VCalendar from 'v-calendar';
+import Paginate from 'vuejs-paginate'
 
 Vue.config.productionTip = false;
 
 Vue.prototype.$http = axios;
+Vue.component('paginate', Paginate)
+
+Vue.use(VCalendar, {
+    componentPrefix: 'vc'
+})
 
 new Vue({
   router,
