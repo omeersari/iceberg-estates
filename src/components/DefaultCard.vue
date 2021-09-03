20111c3dbf4fd9ed291ad5f981a51e14689272ab
omeersari <omeersari@gmail.com>
2021-09-03 22:49:55 +0300

    css changes are applied. favicon is changed.

---
+++ src/components/DefaultCard.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="def-card">
+    <div class="title">
+        {{ title }}
+    </div>
+    <div class="body">
+        {{body}}
+    </div>
+    <div class="footer">
+        {{footer}}
+    </div>
+</div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        title : {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: false,
+            default: "0"
+        },
+        footer: {
+            type: String,
+            required: false
+        }
+
+    }
+}
+</script>
