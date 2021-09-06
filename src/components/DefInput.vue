5eedc908d8cb35c4d475696bb38ce97093b2e031
omeersari <omeersari@gmail.com>
2021-09-06 12:53:53 +0300

    input component.

---
+++ src/components/DefInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-input">
+    <label for=""> {{ placeholder }}</label>
+    <input :type="type" />
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
